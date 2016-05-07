764aa0d3c891001520a859e412bb6b566d67c149
mzp <mzpppp@gmail.com>
2016-05-07 22:05:06 +0900

    generate preview/thumbnail

--- qltext-jp/GeneratePreviewForURL.c
+++ qltext-jp/GeneratePreviewForURL.c
@@ -1,20 +1,32 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
+#import <Cocoa/Cocoa.h>
+#import "NSData+DetectEncoding.h"
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
 /* -----------------------------------------------------------------------------
-   Generate a preview for file
+ Generate a preview for file
 
-   This function's job is to create preview for designated file
-   ----------------------------------------------------------------------------- */
+ This function's job is to create preview for designated file
+ ----------------------------------------------------------------------------- */
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
-    return noErr;
+    @autoreleasepool {
+        NSStringEncoding encoding = [NSData detectEncodingOfURL:(__bridge NSURL*)url];
+        if(encoding != 0) {
+            NSDictionary *props = @{
+                (NSString *)kQLPreviewPropertyStringEncodingKey : @(CFStringConvertNSStringEncodingToEncoding(encoding))
+            };
+            QLPreviewRequestSetURLRepresentation(preview, url, kUTTypePlainText, (__bridge CFDictionaryRef)props);
+            return noErr;
+        } else {
+            return -1;
+        }
+    }
 }
 
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
--- qltext-jp/GenerateThumbnailForURL.c
+++ qltext-jp/GenerateThumbnailForURL.c
@@ -1,20 +1,37 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
+#import <Cocoa/Cocoa.h>
+#import "NSData+DetectEncoding.h"
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
 
 /* -----------------------------------------------------------------------------
-    Generate a thumbnail for file
+ Generate a thumbnail for file
 
-   This function's job is to create thumbnail for designated file as fast as possible
-   ----------------------------------------------------------------------------- */
+ This function's job is to create thumbnail for designated file as fast as possible
+ ----------------------------------------------------------------------------- */
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
-    // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
-    return noErr;
+    @autoreleasepool {
+        NSStringEncoding encoding = [NSData detectEncodingOfURL:(__bridge NSURL*)url];
+        NSDictionary *previewProperties = @{
+            (NSString *)kQLPreviewPropertyStringEncodingKey : @(CFStringConvertNSStringEncodingToEncoding(encoding))
+        };
+
+        NSDictionary *properties = @{
+        };
+
+        QLThumbnailRequestSetThumbnailWithURLRepresentation(
+                                                            thumbnail,
+                                                            url,
+                                                            kUTTypePlainText,
+                                                            (__bridge CFDictionaryRef)previewProperties,
+                                                            (__bridge CFDictionaryRef)properties);
+        return noErr;
+    }
 }
 
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail)
